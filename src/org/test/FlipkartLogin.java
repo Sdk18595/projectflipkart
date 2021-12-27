0ae25ee1c6b68e2921ed782e55af993012b826ae
Sdk18595 <dineshkarthi18595@gmail.com>
2021-12-26 21:41:05 -0500

    karthick code

--- src/org/test/FlipkartLogin.java
+++ src/org/test/FlipkartLogin.java
@@ -1,5 +1,40 @@
 package org.test;
 
-public class FlipkartLogin {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipkartLogin {
+	
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SDK\\eclipse-workspace\\Selenium\\Browser\\chromedriver.exe");
+		
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	
+		WebElement clse = driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']"));
+		clse.click();
+		
+		WebElement txtbox = driver.findElement(By.name("q"));
+		txtbox.sendKeys("sumsung mobiles");
+		
+		
+		WebElement cl = driver.findElement(By.className("L0Z3Pu"));
+		cl.click();
+		
+		
+		
+
+}
+
+	
 }
